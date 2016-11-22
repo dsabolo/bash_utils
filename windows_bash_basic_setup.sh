60037b01728a1559644f12f607e0dd1423e211d7
Diego Sabolo <diego.sabolo@gmail.com>
2016-11-22 10:19:41 -0300

    Update windows_bash_basic_setup.sh

--- windows_bash_basic_setup.sh
+++ windows_bash_basic_setup.sh
@@ -6,30 +6,30 @@
 #
 ####################################################################
 
-//Install & Config git
+# Install & Config git
 sudo apt-get install git
 git config --global user.name "dsabolo"
 git config --global user.mail "diego.sabolo@gmail.com"
 ssh-keygen -t rsa -b 4096 -C "diego.sabolo@gmail.com"
 
 
-//Install & Config LAMPP for Drupal Development (By installing drupal7 packages, I'm getting all LAMPP dependencies)
+# Install & Config LAMPP for Drupal Development (By installing drupal7 packages, I'm getting all LAMPP dependencies)
 sudo apt-get install drupal7 drush 
 sudo service mysql start
 sudo apachectl start
 
 
-//Installing Composer
+# Installing Composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 
-//Installing Ruby & Compass
+# Installing Ruby & Compass
 sudo apt-get install ruby rubygems-integration
 sudo apt-get install ruby-sass
 sudo apt-get install ruby-compass
 
 
-//Installing Node / NPM
+# Installing Node / NPM
 sudo apt-get install nodejs
 sudo apt-get install npm
 
